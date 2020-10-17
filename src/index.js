582f0483e0b8d554f862535254f413480bb51068
emigiusto <51482521+emigiusto@users.noreply.github.com>
2020-10-17 16:00:43 +0200

    First-commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require('firebase')
+require('firebase/firestore')
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyCww5j1vlJCTHwE-zDAadKjYTwDjFvxLBI",
+    authDomain: "load-balancer-59c48.firebaseapp.com",
+    databaseURL: "https://load-balancer-59c48.firebaseio.com",
+    projectId: "load-balancer-59c48",
+    storageBucket: "load-balancer-59c48.appspot.com",
+    messagingSenderId: "842465811688",
+    appId: "1:842465811688:web:70878da44975c42001bd5d",
+    measurementId: "G-G5XWN9FZ5P"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
